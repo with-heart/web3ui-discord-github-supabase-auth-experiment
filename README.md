94acc762583efe9da9e889d1380b6b9e3bacf8ed
with-heart <with.heart+git@pm.me>
2022-01-06 12:52:49 -0500

    chore: initialize repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# `web3-ui-discord-github-supabase-auth-experiment`
+
+This repo is an experiment for authenticating a user with Discord and GitHub and
+storing the information in Supabase.
+
+This experiment is a way for me to learn some essentials about the `web3-ui`,
+Discord, GitHub, and Supabase APIs, and how to tie all of the information
+together in order to authenticate a user and store their account information for
+each service in Supabase.
---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>web3-ui+discord+github+supabase auth experiment</title>
+        <meta
+          name="description"
+          content="A learning experiment by with-heart"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Hello world!</main>
+    </div>
+  )
+}
+
+export default Home
